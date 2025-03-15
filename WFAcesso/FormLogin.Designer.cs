639ae8c3a768703917fdc5dd164c2b68fc5b6b2c
THEODORO MARTINS DE FREITAS <theomartins137@gmail.com>
2025-03-14 22:16:11 -0300

    feat: Tela de Login com autenticação

---
+++ WFAcesso/FormLogin.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WFAcesso
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbl_usuario = new Label();
+            txt_usuario = new TextBox();
+            txt_senha = new TextBox();
+            lbl_senha = new Label();
+            btn_logar = new Button();
+            SuspendLayout();
+            // 
+            // lbl_usuario
+            // 
+            lbl_usuario.AutoSize = true;
+            lbl_usuario.Location = new Point(29, 30);
+            lbl_usuario.Name = "lbl_usuario";
+            lbl_usuario.Size = new Size(50, 15);
+            lbl_usuario.TabIndex = 0;
+            lbl_usuario.Text = "Usuário:";
+            // 
+            // txt_usuario
+            // 
+            txt_usuario.Location = new Point(29, 48);
+            txt_usuario.Name = "txt_usuario";
+            txt_usuario.Size = new Size(100, 23);
+            txt_usuario.TabIndex = 1;
+            txt_usuario.KeyDown += this.txt_usuario_KeyDown;
+            // 
+            // txt_senha
+            // 
+            txt_senha.Location = new Point(29, 104);
+            txt_senha.Name = "txt_senha";
+            txt_senha.PasswordChar = '*';
+            txt_senha.Size = new Size(100, 23);
+            txt_senha.TabIndex = 3;
+            // 
+            // lbl_senha
+            // 
+            lbl_senha.AutoSize = true;
+            lbl_senha.Location = new Point(29, 86);
+            lbl_senha.Name = "lbl_senha";
+            lbl_senha.Size = new Size(42, 15);
+            lbl_senha.TabIndex = 2;
+            lbl_senha.Text = "Senha:";
+            // 
+            // btn_logar
+            // 
+            btn_logar.Location = new Point(29, 146);
+            btn_logar.Name = "btn_logar";
+            btn_logar.Size = new Size(100, 23);
+            btn_logar.TabIndex = 4;
+            btn_logar.Text = "Logar";
+            btn_logar.UseVisualStyleBackColor = true;
+            btn_logar.Click += btn_logar_Click;
+            // 
+            // FormLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(164, 200);
+            Controls.Add(btn_logar);
+            Controls.Add(txt_senha);
+            Controls.Add(lbl_senha);
+            Controls.Add(txt_usuario);
+            Controls.Add(lbl_usuario);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FormLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            Load += FormLogin_Load_1;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbl_usuario;
+        private TextBox txt_usuario;
+        private TextBox txt_senha;
+        private Label lbl_senha;
+        private Button btn_logar;
+    }
+}
---
+++ WFAcesso/FormLogin.cs
@@ -0,0 +1,86 @@
+namespace WFAcesso
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+            AdicionarLista();
+        }
+
+        private void btn_logar_Click(object sender, EventArgs e)
+        {
+
+            Usuario user1 = new Usuario();
+            user1.Login = txt_usuario.Text;
+            user1.Senha = txt_senha.Text;
+
+
+
+            foreach (Usuario us in Usuario.ListaUsuarios)
+            {
+                if (us.Login == txt_usuario.Text )
+                {
+                    if (us.Senha == txt_senha.Text)
+                    {
+                        MessageBox.Show("Usuario Autenticado com Sucesso!","Sucesso!", MessageBoxButtons.OK,MessageBoxIcon.Information);
+                        return;
+                    }
+                }
+            }
+            MessageBox.Show("Usuario Não Autenticado!","Erro!", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            return;
+
+
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+            AdicionarLista();
+
+        }
+
+        private void FormLogin_Load_1(object sender, EventArgs e)
+        {
+
+        }
+      
+        public void AdicionarLista()
+        {
+            Usuario us = new Usuario();
+            us.Codigo = 1;
+            us.Login = "admin";
+            us.Senha = "admin";
+            us.DataCadastro = Convert.ToDateTime("01/01/2000");
+            //us.DataCadastro = DateTime.Parse("01/01/2000");
+            Usuario.ListaUsuarios.Add(us);
+
+            us = new Usuario();
+            us.Codigo = 2;
+            us.Login = "user";
+            us.Senha = "user";
+            us.DataCadastro = Convert.ToDateTime("01/01/2010");
+            //us.DataCadastro = DateTime.Parse("01/01/2010");
+            Usuario.ListaUsuarios.Add(us);
+        }
+        private void txt_usuario_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txt_senha.Focus();
+            }
+        }
+
+        private void txt_senha_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btn_logar_Click(sender, e);
+            }
+        }
+
+
+    }
+
+
+}
